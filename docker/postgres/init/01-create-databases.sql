9fb26e11de3d6c45b2596e21ca0b3e8c4efa3978
PRESSO <caff1nepill@gmail.com>
2026-03-26 11:27:07 +0900

    ci(deploy): add dockerhub-based production deployment

--- docker/postgres/init/01-create-databases.sql
+++
@@ -1,9 +0,0 @@
-SELECT 'CREATE DATABASE dev_db'
-WHERE NOT EXISTS (
-    SELECT FROM pg_database WHERE datname = 'dev_db'
-)\gexec
-
-SELECT 'CREATE DATABASE test_db'
-WHERE NOT EXISTS (
-    SELECT FROM pg_database WHERE datname = 'test_db'
-)\gexec
